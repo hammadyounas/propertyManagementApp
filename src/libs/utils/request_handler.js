c740a768f1b399e1374a9f2c2253eb834c1e0bdd
Hammad Younus <m.hammadyounas@outlook.com>
2024-12-02 14:16:30 +0500

    changes in request handler

--- src/libs/utils/request_handler.js
+++ src/libs/utils/request_handler.js
@@ -1,39 +1,46 @@
 import axios from "axios";
 import { API_PREFIX, API_URL } from "../../configs";
 
+const apiClient = axios.create({
+  baseURL: `${API_URL}/${API_PREFIX}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // Use this if cookies or credentials are involved
+});
+
 export const postRequest = async (url, data) => {
-    try {
-      const response = await axios.post(`${API_URL}/${API_PREFIX}/${url}`, data);
-      return response.data;
-    } catch (error) {
-      throw error; // Rethrow the error for further handling
-    }
-  };
+  try {
+    const response = await apiClient.post(url, data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const getRequest = async (url) => {
   try {
-    const response = await axios.get(`${API_URL}/${API_PREFIX}/${url}`);
+    const response = await apiClient.get(url);
     return response.data;
   } catch (error) {
     throw error;
   }
-}
+};
 
 export const putRequest = async (url, data) => {
   try {
-    const response = await axios.put(`${API_URL}/${API_PREFIX}/${url}`, data);
+    const response = await apiClient.put(url, data);
     return response.data;
   } catch (error) {
     throw error;
   }
-}
+};
 
 export const deleteRequest = async (url) => {
   try {
-    const response = await axios.delete(`${API_URL}/${API_PREFIX}/${url}`);
+    const response = await apiClient.delete(url);
     return response.data;
   } catch (error) {
     throw error;
   }
-}
-  
+};
