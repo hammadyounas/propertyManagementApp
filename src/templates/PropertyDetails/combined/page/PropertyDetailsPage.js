80fbf025e74ed705a42c7838fd934dedcfe6ffcc
Hammad Younus <m.hammadyounas@outlook.com>
2024-12-06 20:24:57 +0500

    minor changes

--- src/templates/PropertyDetails/combined/page/PropertyDetailsPage.js
+++ src/templates/PropertyDetails/combined/page/PropertyDetailsPage.js
@@ -15,8 +15,8 @@ import Location from "../molecules/LocationUIContainer";
 
 const PropertyDetailsPage = () => {
   const { loading, propertyDetails, push, rows, documentDataRows, salesperosonDataRows } = usePropertyDetails();
- 
-const iframeHtml = `
+
+  const iframeHtml = `
 <iframe
   src="${propertyDetails?.location_map_url || "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28968.491278753427!2d67.02223737519975!3d24.827573438137424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e84755a83d3%3A0xf49fbe49c4a0095e!2sibex.%20Pakistan!5e0!3m2!1sen!2s!4v1731342989428!5m2!1sen!2s"}"
   width="600"
@@ -54,19 +54,9 @@ const iframeHtml = `
         <Details details={propertyDetails?.description} />
         <Amenities amenities={propertyDetails?.amenities} />
         <Documents documentDataRows={documentDataRows} />
-        <Salesperson salesperosonDataRows={salesperosonDataRows}/>
-        <Notes rows={rows}/>
-        <Location locationMapUrl={propertyDetails?.location_map_url}/>
-        {/* <div className="my-4">
-          <h2 className="text-lg">Location</h2>
-          <div className="flex flex-wrap my-4 w-full">
-            <div
-              id="map"
-              style={{ textAlign: "center", width:'100%' }}
-              dangerouslySetInnerHTML={{ __html: iframeHtml }}
-            />
-          </div>
-        </div> */}
+        <Salesperson salesperosonDataRows={salesperosonDataRows} />
+        <Notes rows={rows} />
+        <Location locationMapUrl={propertyDetails?.location_map_url} />
         <div className="flex justify-center md:justify-end mt-12">
           <Button
             onClick={() => push("/properties")}
