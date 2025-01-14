631131564efe65af3bf548eee8523b0a263ea894
hammadyounas <m.hammadyounas@outlook.com>
2025-01-14 21:47:23 +0500

    Update AdditionalInformationUI.js

--- src/templates/CreateInvoice/ui/molecules/AdditionalInformationUI.js
+++ src/templates/CreateInvoice/ui/molecules/AdditionalInformationUI.js
@@ -4,21 +4,31 @@ const AdditionalInformationUI = ({ register, loading }) => {
   return (
     <div className="mt-8">
       <h6>Additional Information</h6>
-      <div className="my-4">
-        <div className="flex flex-wrap justify-between">
-          <div className="w-full md:w-[49%] mt-1">
-            <Textarea
-              name="notes"
-              label="Additional Notes"
-              type="text"
-              register={register}
-              placeholder="Any additional information for the client (e.g., late payment policies, disclaimers)."
-              row={5}
-              disabled={loading}
-            />
-          </div>
+      <div className="my-4 space-y-4">
+        <div className="flex justify-between items-center">
+          <p className="font-medium">Total commission payable</p>
+          <p className="font-medium">$1,550.00</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="font-medium">Commission amount</p>
+          <p className="font-medium">$1348.12</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="font-medium">Plus GST 5% (755399938)</p>
+          <p className="font-medium">$67.12</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="font-medium">Plus QST 9.975% (1226981027)</p>
+          <p className="font-medium">$134.47</p>
+        </div>
+        <div className="flex justify-between items-center font-bold border-t pt-4">
+          <p>Total:</p>
+          <p>$1,550.00</p>
         </div>
       </div>
+      <p className="text-gray-700 mt-6">
+        Thank you and we look forward to doing business together again!
+      </p>
     </div>
   );
 };
