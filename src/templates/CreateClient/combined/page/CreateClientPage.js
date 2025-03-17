18950aead2757adbb27d796518398880d8ef529b
Hammad Younus <m.hammadyounas@outlook.com>
2025-03-17 15:22:08 +0500

    add csv file upload functionality in clients form

--- src/templates/CreateClient/combined/page/CreateClientPage.js
+++ src/templates/CreateClient/combined/page/CreateClientPage.js
@@ -21,6 +21,14 @@ const CreateClientPage = () => {
     clientTypes,
     preferredCommunicationChannels,
     salesPersons,
+    handleFileUpload,
+    handleRemoveCSV,
+    csvData,
+    getInputProps,
+    getRootProps,
+    acceptedFiles,
+    inputType,
+    setInputType,
   } = useForm();
   return (
     <FormUI
@@ -42,6 +50,14 @@ const CreateClientPage = () => {
       clientTypes={clientTypes}
       preferredCommunicationChannels={preferredCommunicationChannels}
       salesPersons={salesPersons}
+      handleFileUpload={handleFileUpload}
+      handleRemoveCSV={handleRemoveCSV}
+      csvData={csvData}
+      getInputProps={getInputProps}
+      getRootProps={getRootProps}
+      acceptedFiles={acceptedFiles}
+      inputType={inputType}
+      setInputType={setInputType}
     />
   );
 };
