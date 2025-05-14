d61cbe99d8c72686e805235246a0b6dcbe735f12
Hammad Younus <m.hammadyounas@outlook.com>
2025-05-14 13:29:37 +0500

    change broker pagination UI

--- src/templates/SalesTeamListing/combined/page/SalesTeamListingPage.js
+++ src/templates/SalesTeamListing/combined/page/SalesTeamListingPage.js
@@ -3,6 +3,7 @@ import { Icon } from "@iconify/react";
 import ReactPaginate from "react-paginate";
 import useSalesTeam from "../../functionality/page/useSalesTeam";
 import ConfirmDeleteModal from "../../../../components/ui/molecules/ConfirmDeleteModal";
+import PaginationUI from "../../../../components/ui/molecules/PaginationUI";
 
 const SalesTeamListingPage = () => {
   const {
@@ -36,26 +37,23 @@ const SalesTeamListingPage = () => {
         loading={loading}
         openDeleteModal={openDeleteModal}
         users={users}
-        setStatusFilter={setStatusFilter}  // ✅ Make sure this is passed
+        setStatusFilter={setStatusFilter} // ✅ Make sure this is passed
         statusFilter={statusFilter}
         setSelectedFilter={setSelectedFilter}
         selectedFilter={selectedFilter}
       />
-      <div className={`flex w-full justify-end mt-2 items-center ${users?.length <= pageSize && "hidden"}`}>
-        <ReactPaginate
-          previousLabel={<Icon icon="heroicons-outline:chevron-left" />}
-          nextLabel={<Icon icon="heroicons-outline:chevron-right" />}
-          breakLabel={"..."}
-          pageCount={Math.ceil(users.length / pageSize)} // Correctly calculate the number of pages
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
+      <div
+        className={`flex w-full justify-end mt-2 items-center ${
+          users?.length <= pageSize && "hidden"
+        }`}
+      >
+        <PaginationUI
+          pageCount={Math.ceil(users.length / pageSize)}
           onPageChange={({ selected }) => handlePageChange(selected)}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}
           initialPage={currentPage - 1}
         />
       </div>
+      
       <ConfirmDeleteModal
         isOpen={showDeleteModal}
         onClose={closeDeleteModal}
