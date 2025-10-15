346778473208c921507ead78ab83fb80dba217e2
Hammad Younus <m.hammadyounas@outlook.com>
2025-10-15 20:47:26 +0500

    remove import template button

--- src/templates/CreateTemplate/ui/TextEditorUI.js
+++ src/templates/CreateTemplate/ui/TextEditorUI.js
@@ -70,13 +70,13 @@ export default function TemplateEditorUI({
                   className="text-sm w-full"
                 />
               </div> */}
-              <button
+              {/* <button
                 type="button"
                 onClick={handleTriggerImport}
                 className="w-full sm:w-auto px-4 py-2 bg-gray-100 dark:bg-slate-700 text-gray-800 dark:text-slate-100 rounded-lg hover:bg-gray-200 dark:hover:bg-slate-600 transition-colors flex items-center justify-center space-x-2"
               >
                 <span>Import Template</span>
-              </button>
+              </button> */}
               <button
                 onClick={() => {
                   const templateData = {
